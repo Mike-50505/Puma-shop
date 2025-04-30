e65a988bde045742f3f8552738704e1209ffaa85
Miguel <chettosconsalsa5@gmail.com>
2025-04-30 11:51:18 -0600

    segundo commit

--- Puma_Shop/urls.py
+++ Puma_Shop/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('healthz', health_check),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
