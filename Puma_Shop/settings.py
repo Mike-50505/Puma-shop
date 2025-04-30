4b86d2ea7f7dc2459804776388431eecb81a24e5
Miguel <chettosconsalsa5@gmail.com>
2025-04-30 12:24:45 -0600

    tercer commit

--- Puma_Shop/settings.py
+++ Puma_Shop/settings.py
@@ -90,6 +90,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'store/static')
